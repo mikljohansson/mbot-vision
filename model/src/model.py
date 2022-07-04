Fixed loss function masking

model/src/model.py
@@ -117,7 +117,7 @@ class MBotVisionModel(nn.Module):
         self.mean2x = torch.nn.Parameter(2. * torch.tensor([0.485, 0.456, 0.406]).reshape(-1, 1, 1), requires_grad=False)
         self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225]).reshape(-1, 1, 1), requires_grad=False)
 
-        self.initialize_weights()
+        #self.initialize_weights()
 
     def initialize_weights(self):
         for m in self.modules():

model/src/train.py
@@ -99,7 +99,7 @@ def upsample_like(t, like, mode='bilinear'):
 
 def calculate_loss(outputs, targets, unknown_mask, z_loss=1e-4):
     alpha_loss = F.binary_cross_entropy_with_logits(outputs, targets, reduction='none')
-    loss = (alpha_loss * unknown_mask).mean()
+    loss = (alpha_loss * (1. - unknown_mask)).mean()
 
     # Add a separate loss to keep the logits from drifting too far from zero and encourage the
     # logits to be normalized log-probabilities. This might also help prevent NaN's